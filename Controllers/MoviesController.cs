9c10c455c2422b69043a2c86a0de5c882a243cfb
LazySloth <ekeles87@gmail.com>
2017-08-16 15:58:21 +0300

    Before data

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,10 +10,20 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+            var movies = GetMovies();
+
+            return View(movies);    
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+               new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
     }
 }
